721fb748e90de85d793d9e28f46f13ad790561d4
zakdok <zakdok@tinto.co.kr>
2020-07-08 13:33:20 +0900

    js 수정

--- assets/js/researchBlock.js
+++ assets/js/researchBlock.js
@@ -144,7 +144,6 @@ function openMoodBoard() {
         openModalPopup('moodBoardPopup');
         setTimeout(() => {
             createMoodBoardItemList();
-            waitForImages();
         }, 250);
     });
 }
